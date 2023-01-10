75294451c457b14fe43511d1ecb056ac9ed99b25
Evandro <evandrocalado07@gmail.com>
2023-01-10 00:26:11 -0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import userIcon from "../../assets/images/user-icon.png";
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useRef } from "react";
+import { useSelector } from "react-redux";
 
 const nav__links = [
   {
@@ -24,65 +25,69 @@ const nav__links = [
 
 const Header = () => {
   const menuRef = useRef(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const menuToggle = () => menuRef?.current?.classList?.toggle("active__menu");
 
   return (
-    <header className="header">
-      <Container>
-        <Row>
-          <div className="nav__wrapper">
-            <div className="logo">
-              <img src={logo} alt="Logo" />
-              <div>
-                <h1>Multimart</h1>
+    <>
+      <div className="header__back"></div>
+      <header className="header">
+        <Container>
+          <Row>
+            <div className="nav__wrapper">
+              <div className="logo">
+                <img src={logo} alt="Logo" />
+                <div>
+                  <h1>Multimart</h1>
+                </div>
               </div>
-            </div>
 
-            <div className="navgation" ref={menuRef} onClick={menuToggle}>
-              <ul className="menu">
-                {nav__links.map((item, index) => {
-                  return (
-                    <li key={index} className="nav__item">
-                      <NavLink
-                        to={item.path}
-                        className={(navClass) =>
-                          navClass.isActive ? "nav__active" : ""
-                        }
-                      >
-                        {item.display}
-                      </NavLink>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div className="nav__icons">
-              <span className="fav__icon">
-                <i className="ri-heart-line"></i>
-                <span className="badge">1</span>
-              </span>
-              <span className="cart__icon">
-                <i className="ri-shopping-bag-line"></i>
-                <span className="badge">1</span>
-              </span>
-              <span>
-                <motion.img
-                  whileTap={{ scale: 1.2 }}
-                  src={userIcon}
-                  alt="User Icon"
-                />
-              </span>
-              <div className="mobile__menu">
-                <span onClick={menuToggle}>
-                  <i className="ri-menu-line"></i>
+              <div className="navgation" ref={menuRef} onClick={menuToggle}>
+                <ul className="menu">
+                  {nav__links.map((item, index) => {
+                    return (
+                      <li key={index} className="nav__item">
+                        <NavLink
+                          to={item.path}
+                          className={(navClass) =>
+                            navClass.isActive ? "nav__active" : ""
+                          }
+                        >
+                          {item.display}
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="nav__icons">
+                <span className="fav__icon">
+                  <i className="ri-heart-line"></i>
+                  <span className="badge">1</span>
                 </span>
+                <span className="cart__icon">
+                  <i className="ri-shopping-bag-line"></i>
+                  <span className="badge">{totalQuantity}</span>
+                </span>
+                <span>
+                  <motion.img
+                    whileTap={{ scale: 1.2 }}
+                    src={userIcon}
+                    alt="User Icon"
+                  />
+                </span>
+                <div className="mobile__menu">
+                  <span onClick={menuToggle}>
+                    <i className="ri-menu-line"></i>
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </Row>
-      </Container>
-    </header>
+          </Row>
+        </Container>
+      </header>
+    </>
   );
 };
 
--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,9 +1,131 @@
-import React from 'react';
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import Helmet from "../Helmet/Helmet";
+import CommonSection from "../Ui/CommonSection";
+import "../styles/Shop.css";
+import { useState } from "react";
+import productsData from "../../assets/data/products";
+import ProductsList from "../Ui/ProductsList";
 
 const Shop = () => {
-  return ( 
-    <div>Shop</div>
-   );
-}
- 
+  const [products, setProducts] = useState(productsData);
+
+  const heandleFilter = (e) => {
+    const filterValue = e.target.value;
+    console.log(products);
+
+    if (filterValue === "sofa") {
+      const filteredProducts = productsData.filter(
+        (item) => item.category === "sofa"
+      );
+
+      setProducts(filteredProducts);
+    }
+
+    if (filterValue === "mobile") {
+      const filteredProducts = productsData.filter(
+        (item) => item.category === "mobile"
+      );
+
+      setProducts(filteredProducts);
+    }
+
+    if (filterValue === "chair") {
+      const filteredProducts = productsData.filter(
+        (item) => item.category === "chair"
+      );
+
+      setProducts(filteredProducts);
+    }
+
+    if (filterValue === "watch") {
+      const filteredProducts = productsData.filter(
+        (item) => item.category === "watch"
+      );
+
+      setProducts(filteredProducts);
+    }
+
+    if (filterValue === "wireless") {
+      const filteredProducts = productsData.filter(
+        (item) => item.category === "wireless"
+      );
+
+      setProducts(filteredProducts);
+    }
+
+    if (filterValue === "all") {
+      setProducts(productsData);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value;
+
+    const searchProducts = productsData.filter((item) =>
+      item.productName
+        .toLocaleLowerCase()
+        .includes(searchValue.toLocaleLowerCase())
+    );
+
+    setProducts(searchProducts);
+  };
+
+  return (
+    <Helmet title="shop">
+      <CommonSection title="Products" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3" md="6">
+              <div className="filter__widget">
+                <select onChange={heandleFilter}>
+                  <option value="all">Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="3" md="6" className="text-end">
+              <div className="filter__widget">
+                <select>
+                  <option>Sort By</option>
+                  <option value="ascending">Ascending</option>
+                  <option value="descending">Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="6" md="12">
+              <div className="search__box">
+                <input
+                  onChange={handleSearch}
+                  type="text"
+                  placeholder="Search..."
+                />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {products.length === 0 ? (
+              <h1 className="text-center fs-1">No products are found !</h1>
+            ) : (
+              <ProductsList data={products} />
+            )}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
 export default Shop;
